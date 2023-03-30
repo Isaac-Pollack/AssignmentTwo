d43e44390fdd0bf86869ec6d7517b3fcb5a47a6a
Isaac Pollack <pollackisaac@gmail.com>
2023-03-30 12:47:28 +0900

    Playing Around
    - Adding Buttons

--- AssignmentTwo/ContentView.swift
+++ AssignmentTwo/ContentView.swift
@@ -8,29 +8,35 @@
 import SwiftUI
 
 struct Checklist: Hashable {
-    var name:String = "<unknown>"
+    //This will be used for JSON down the track
+    var name: String = "<unknown>"
 }
 
 struct ContentView: View {
+    //Default Checklists
     @State var myChecklists = [
         Checklist(name:"Groceries"),
-        Checklist(name:"Chores"),
-        Checklist(name:"Packing List")
     ]
     var body: some View {
         NavigationView {
+            //Navlink to each checklist created
             List {
-                ForEach(myChecklists, id:\.self) {
-                    checklist in
-                    Text(checklist.name)
+                ForEach($myChecklists, id:\.self) {
+                    $checklist in
+                    NavigationLink(destination: DetailView(name: $checklist.name)) {
+                        Text(checklist.name)
+                    }
                 }.onDelete {
+                    //Remove the item tapped
                     indecs in
                     myChecklists.remove(atOffsets: indecs)
                 }
-            }.navigationBarItems(leading: EditButton(), trailing: Button("+"){
+            }.navigationBarItems(leading: EditButton(), trailing: Button("+") {
+                //Append a new checklist item when clicked
                 let newChecklist = Checklist(name: "New Checklist\(myChecklists.count)")
                 myChecklists.append(newChecklist)
             }).navigationTitle("My Checklists")
+                .navigationBarTitleDisplayMode(.automatic)
         }
     }
 }
--- AssignmentTwo/DetailView.swift
+++ AssignmentTwo/DetailView.swift
@@ -7,48 +7,65 @@
 
 import SwiftUI
 
+struct Item: Hashable {
+    //This will be used for JSON down the track
+    var name: String = "<unknown>"
+}
+
 struct DetailView: View {
     @Binding var name: String
     @State var newName = ""
     @State var displayName = ""
     @State var originName = ""
     
+    @State var isEditing = false
+    
+    @State var ChecklistItem = [
+        Item(name:"Apple")]
+    
     var body: some View {
-        VStack {
-            TitleView(title: displayName, icon: "person.circle")
-            Text("You can enter a new name below:")
-            TextField("<New Name>", text:$newName)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-            
-            HStack {
-                Button("Change") {
-                    if (newName != "") {
-                        displayName = newName
+        NavigationView {
+                VStack {
+                    Text(displayName).font(.largeTitle)
+                    
+                    //TextField("<New Name>", text:$newName)
+                    List {
+                        ForEach($ChecklistItem, id:\.self) {
+                            $Item in
+                            Text(Item.name)
+                        }.navigationBarItems(
+                            leading:
+                                HStack {
+                                    EditButton()
+                                },
+                            trailing:
+                                HStack {
+                                    Button("Apply") {
+                                        if (newName != "") {
+                                            displayName = newName
+                                        }
+                                    }
+                                    Button("Reset") {
+                                        displayName = originName
+                                    }.foregroundColor(Color.red)
+                                })
                     }
                 }
-                Spacer()
-                Button("Cancel") {
-                    displayName = originName
-                }
-            }.padding()
-            Spacer()
-        }
-        
-        .padding()
-        .onAppear {
-            //Set initial value to below preview/binded 'name'
-            originName = name
-            displayName = name
-        }
-        .onDisappear {
-            //When we leave the view; set the name to the changed value
-            name = displayName
+            }
+            .onAppear {
+                //Set initial value to below preview/binded 'name'
+                originName = name
+                displayName = name
+            }
+            .onDisappear {
+                //When we leave the view; set the name to the changed value
+                name = displayName
+            }
         }
     }
-}
 
 struct DetailView_Previews: PreviewProvider {
     static var previews: some View {
-        DetailView(name:.constant("Isaac Pollack"))
+        DetailView(name:.constant("Groceries"))
     }
 }
